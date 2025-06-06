267dea677c5adcc2086a7a709e4d64603661ab0c
ReneGarBer <renesoftdev@gmail.com>
2025-06-06 17:15:48 -0600

    Remover dbt_app y extract_app de la raiz

--- dbt_app/models/marts/promedios_mensuales_ndvi_sw.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	r.*
-	,l.ndvi
-FROM
-	{{source('promedios_mensuales','promedios_mensuales_ndvi')}} l 
-	JOIN
-	{{source('promedios_mensuales','promedios_mensuales_surfaceweather')}} r
-	ON l.anio = r.anio AND l.mes = r.mes AND l.municipio_num = r.municipio_num
-ORDER BY r.anio,r.mes,r.municipio_num
