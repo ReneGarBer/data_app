017a9c0a4b548a7d94a3752b4199ecf84d270142
ReneGarBer <renesoftdev@gmail.com>
2025-06-23 14:26:14 -0600

    tests.sh

--- apps/dbt_app/tests/tests.sh
+++ apps/dbt_app/tests/tests.sh
@@ -1,16 +1,17 @@
 #!bin/bash
 
 cd ../src/
-dbt debug -t rfnd > result.log || true
+ls -ltr
+# dbt debug -t rfnd > result.log || true
 
-if grep -q "All checks passed!" result.log; then
-    echo "not_failures=true" >> "$GITHUB_OUTPUT"
-else
-    echo "not_failures=false" >> "$GITHUB_OUTPUT"
-fi
+# if grep -q "All checks passed!" result.log; then
+#     echo "not_failures=true" >> "$GITHUB_OUTPUT"
+# else
+#     echo "not_failures=false" >> "$GITHUB_OUTPUT"
+# fi
 
-RESULT=$(cat result.log){
-    echo "test_output<<EOF"
-    echo "$RESULT"
-    echo "EOF"
-} >> "$GITHUB_OUTPUT"
+# RESULT=$(cat result.log){
+#     echo "test_output<<EOF"
+#     echo "$RESULT"
+#     echo "EOF"
+# } >> "$GITHUB_OUTPUT"
