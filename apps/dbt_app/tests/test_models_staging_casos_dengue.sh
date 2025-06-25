ac3294dbf27d0ba1cf7c1b73610eb7d3458f8b22
ReneGarBer <renesoftdev@gmail.com>
2025-06-24 21:35:33 -0600

    test para el modelo staging_casos_dengue

--- apps/dbt_app/tests/test_models_staging_casos_dengue.sh
+++ apps/dbt_app/tests/test_models_staging_casos_dengue.sh
@@ -1,6 +1,7 @@
 cd ../src/
 
 dbt run --full-refresh -s staging.staging_casos_dengue -t stg
+
 dbt debug test -s staging.staging_casos_dengue -t stg > result.log || true
 
 if grep -q "Completed successfully" result.log; then
