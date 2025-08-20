9ae92a15058f51bebda89588134a886e5de12030
ReneGarBer <renesoftdev@gmail.com>
2025-08-19 19:46:02 -0600

    agregar vistas de mediciones climaticas para analisis

---
+++ apps/dbt_app/src/models/marts/reports/mediciones_climaticas_semanales_v.sql
@@ -0,0 +1,41 @@
+WITH join_region AS (
+    SELECT 
+        d.id_region,
+        d.estado,
+        d.municipio,
+        f.id_fecha_inicial,
+        f.id_tipo,
+        f.medicion
+    FROM {{ref("fac_medicion_climatica")}} f
+        JOIN {{ref("dim_region")}} d ON f.id_region = d.id_region
+    WHERE (f.cadencia = '1 day'::text)
+), join_fecha AS (
+    SELECT 
+        f.id_region,
+        f.estado,
+        f.municipio,
+        d.anio as anio,
+        d.semana_num as semana,
+        d.fecha_completa AS fecha,
+        f.id_tipo,
+        f.medicion
+    FROM join_region f
+    JOIN {{ref("dim_fecha")}} d ON f.id_fecha_inicial = d.id_fecha
+), avg_semanal AS (
+    SELECT
+        id_region,
+        estado,
+        municipio,
+        anio,
+        semana,
+        MIN(fecha) inicio_de_semana,
+        MAX(fecha) fin_de_semana,
+        id_tipo,
+        ROUND(AVG(medicion),4) as promedio_semanal
+    FROM join_fecha
+    GROUP BY 1,2,3,4,5,8
+)
+SELECT 
+    *
+FROM avg_semanal
+ORDER BY 2,3,4,5,8
